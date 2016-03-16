b990b6d8d28c9339f0ea9dc25f30bf859ca5d3f1
Sérgio Santos <sergio.santos@log.pt>
2016-03-16 16:10:00 +0000

    Fix get post ACF meta

--- lib/ACF.php
+++ lib/ACF.php
@@ -122,81 +122,6 @@ class ACF {
 	}
 
 	/**
-	 * Prepare post ACF meta.
-	 *
-	 * @since     1.0.0
-	 * @param     array                $data    Prepared post data.
-	 * @param     \Replicast\Client    $site    Site object.
-	 * @return    array                         Possibly-modified post data.
-	 */
-	public function prepare_post_meta( $data, $site ) {
-
-		if ( empty( $data['replicast'] ) ) {
-			return $data;
-		}
-
-		if ( empty( $data['replicast']['meta'] ) ) {
-			return $data;
-		}
-
-		foreach ( $data['replicast']['meta'] as $key => $meta ) {
-
-			if ( empty( $meta['raw'] ) ) {
-				continue;
-			}
-
-			$field_value = \acf_extract_var( $meta['raw'], 'value' );
-
-			if ( empty( $field_value ) ) {
-				continue;
-			}
-
-			$meta_value  = array();
-			$field_type  = \acf_extract_var( $meta['raw'], 'type' );
-
-			if ( $field_type === 'text' ) {
-				$meta_value = $field_value;
-			}
-
-			if ( $field_type === 'relationship' ) {
-
-				foreach ( $field_value as $related_object ) {
-
-					if ( is_numeric( $related_object ) ) {
-						$related_object = \get_post( $related_object );
-					}
-
-					// Get replicast info
-					$replicast_info = API::get_replicast_info( $related_object );
-
-					// Update object ID
-					if ( ! empty( $replicast_info ) ) {
-						$meta_value[] = $replicast_info[ $site->get_id() ]['id'];
-					}
-
-				}
-
-				if ( ! empty( $meta_value ) ) {
-					$meta_value = maybe_serialize( $meta_value );
-				}
-
-			}
-
-			unset( $data['replicast']['meta'][ $key ] );
-
-			if ( empty( $meta_value ) ) {
-				continue;
-			}
-
-			$data['replicast']['meta'][ $key ][] = $meta_value;
-
-		}
-
-		return $data;
-	}
-
-
-	/**
 	 * Expose \Replicast\ACF protected meta keys.
 	 *
 	 * @since     1.0.0
@@ -244,6 +169,74 @@ class ACF {
 	}
 
 	/**
+	 * Prepare post ACF meta.
+	 *
+	 * @since     1.0.0
+	 * @param     array                $data    Prepared post data.
+	 * @param     \Replicast\Client    $site    Site object.
+	 * @return    array                         Possibly-modified post data.
+	 */
+	public function prepare_post_meta( $data, $site ) {
+
+		if ( empty( $data['replicast'] ) ) {
+			return $data;
+		}
+
+		if ( empty( $data['replicast']['meta'] ) ) {
+			return $data;
+		}
+
+		foreach ( $data['replicast']['meta'] as $key => $meta ) {
+
+			if ( empty( $meta['raw'] ) ) {
+				continue;
+			}
+
+			$field_type  = \acf_extract_var( $meta['raw'], 'type' );
+			$field_value = \acf_extract_var( $meta['raw'], 'value' );
+			$meta_value  = '';
+
+			if ( $field_type === 'text' ) {
+				$meta_value = $field_value;
+			}
+
+			if ( $field_type === 'relationship' ) {
+
+				if ( empty( $field_value ) ) {
+					$field_value = array();
+				}
+
+				foreach ( $field_value as $related_object ) {
+
+					if ( is_numeric( $related_object ) ) {
+						$related_object = \get_post( $related_object );
+					}
+
+					// Get replicast info
+					$replicast_info = API::get_replicast_info( $related_object );
+
+					// Update object ID
+					if ( ! empty( $replicast_info ) ) {
+						$meta_value[] = $replicast_info[ $site->get_id() ]['id'];
+					}
+
+				}
+
+				if ( ! empty( $meta_value ) && is_array( $meta_value ) ) {
+					$meta_value = maybe_serialize( $meta_value );
+				}
+
+			}
+
+			unset( $data['replicast']['meta'][ $key ] );
+
+			$data['replicast']['meta'][ $key ][] = $meta_value;
+		}
+
+		return $data;
+	}
+
+	/**
 	 * Suppress \Replicast\ACF meta keys from update.
 	 *
 	 * @since     1.0.0
@@ -262,9 +255,10 @@ class ACF {
 	 * @see    \Replicast\ACF\relationship_persistence
 	 *
 	 * @since    1.0.0
-	 * @param    $values     array     The values of the field.
+	 * @param    array    $values     The values of the field.
+	 * @param    int      $post_id    The post id.
 	 */
-	public function update_post_meta( $values ) {
+	public function update_post_meta( $values, $post_id ) {
 
 	}
 
