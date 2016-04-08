ef905c5a6e00e1ed048090a92a7c3ebaebc4cee2
Sérgio Santos <sergio.santos@log.pt>
2016-04-08 14:32:10 +0100

    Fix hook name

--- lib/API.php
+++ lib/API.php
@@ -485,10 +485,9 @@ class API {
 		 *
 		 * @since    1.0.0
 		 * @param    array     The values of the field.
-		 * @param    string    The object meta type.
 		 * @param    int       The object ID.
 		 */
-		\do_action( "replicast_update_object_{$meta_type}_meta", $meta, $meta_type, $object->ID );
+		\do_action( "replicast_update_object_meta", $meta, $object->ID );
 
 	}
 
