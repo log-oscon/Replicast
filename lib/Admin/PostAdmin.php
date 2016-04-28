83334275ff7828ae18670724de610f88fc8bcc94
Sérgio Santos <sergio.santos@log.pt>
2016-04-28 19:37:30 +0100

    Remove debug

--- lib/Admin/PostAdmin.php
+++ lib/Admin/PostAdmin.php
@@ -859,15 +859,8 @@ class PostAdmin extends Admin {
 					->then(
 						function ( $response ) use ( $site, $handler, $force_delete ) {
 
-error_log( print_r( $force_delete, true ) );
-error_log( print_r( $site->get_name(), true ) );
-error_log( print_r( $response->getStatusCode(), true ) );
-error_log( print_r( $response->getReasonPhrase(), true ) );
-
 							// Get the remote object data
 							$remote_data = json_decode( $response->getBody()->getContents() );
-
-error_log( print_r( $remote_data, true ) );
 
 							if ( empty( $remote_data ) ) {
 								continue;
